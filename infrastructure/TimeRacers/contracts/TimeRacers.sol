a513305da69a4c0611b236d2fbaafd0aefb759cd
StrixSC <talon.strix@gmail.com>
2022-07-17 00:45:08 -0400

    modifed timeracers.sol

--- infrastructure/TimeRacers/contracts/TimeRacers.sol
+++ infrastructure/TimeRacers/contracts/TimeRacers.sol
@@ -7,15 +7,17 @@ contract TimeRacers {
     uint256 public fee = 0.1 ether;
     mapping (address => uint256) private games;
 
+    constructor() public payable {}
+
     function startGame() public payable {
         require(msg.value >= fee);
-        require(games[msg.sender] == 0, "Game already started...");
+        require(!hasGameInProgress(), "Game already started...");
         games[msg.sender] = now + 1 minutes;
     }
 
-    function submitGuess(uint256 guess) public payable {
-        require(games[msg.sender] != 0, "Game not started...");
-        require(block.timestamp < games[msg.sender], "Too late...");
+    function submitGuess(uint256 guess) public returns (bool) {
+        require(hasGameInProgress(), "Game not started...");
+        require(now < games[msg.sender], "Too late...");
         if (guess == uint256(keccak256(abi.encodePacked(bytes32(uint256(block.timestamp + block.difficulty + (uint256(keccak256(abi.encodePacked(block.coinbase))) / (block.timestamp)))), bytes32(uint256(block.gaslimit + ((uint256(keccak256(abi.encodePacked(msg.sender)))) / (block.timestamp))) + block.number))))) {
             (bool sent, ) = msg.sender.call{value: fee + reward}("");
             if (sent) {
@@ -23,6 +25,20 @@ contract TimeRacers {
             } else {
                 games[msg.sender] = now + 1 minutes;
             }
+            return sent;
         }
+    }
+
+    function reset() public {
+        require(hasGameInProgress(), "Game not started...");
+        games[msg.sender] = 0;
+    }
+    
+    function hasGameInProgress() public view returns (bool) {
+        return (games[msg.sender] != 0);
+    }
+
+    function isSolved() public view returns (bool) {
+        return address(this).balance == 0;
     }
 }
